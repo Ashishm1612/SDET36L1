4cb7c58cdbe06d7d20e164daa041185d43c54888
Ashish <Ashish@DESKTOP-FUQUFQ9>
2022-07-18 23:13:54 -0700

    1st push

---
+++ mavenExample.practice/src/test/java/mavenExample/practice/mavenExampleTest1.java
@@ -0,0 +1,19 @@
+package mavenExample.practice;
+
+import org.testng.annotations.Test;
+
+public class mavenExampleTest1 {
+	@Test
+	public void mavenTest1() {
+		System.out.println("test1 ");
+	}
+	@Test
+	public void mavenTest2() {
+		System.out.println("test2 ");
+	}
+	@Test
+	public void mavenTest3() {
+		System.out.println("test3 ");
+	}
+
+}
