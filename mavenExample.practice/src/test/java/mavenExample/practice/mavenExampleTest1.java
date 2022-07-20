0cec134b17536aa4c4a8cc3f5448b9154cb96c14
Ashish <Ashish@DESKTOP-FUQUFQ9>
2022-07-20 03:46:03 -0700

    hi

--- mavenExample.practice/src/test/java/mavenExample/practice/mavenExampleTest1.java
+++ mavenExample.practice/src/test/java/mavenExample/practice/mavenExampleTest1.java
@@ -3,8 +3,14 @@ package mavenExample.practice;
 import org.testng.annotations.Test;
 
 public class mavenExampleTest1 {
+
+	
 	@Test
 	public void mavenTest1() {
+	String BROWSER 	=System.getProperty("browser");
+		String URL = System.getProperty("url");
+		System.out.println("browser------>" +BROWSER);
+		System.out.println("url------>" +URL);
 		System.out.println("test1 ");
 		System.out.println("test run 1");
 	}
